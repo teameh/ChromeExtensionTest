a935b5ebeb6da9eb6be5e13d726542ef6f2db2f5
Tieme van Veen <tiemevanveen@gmail.com>
2015-02-09 21:17:43 +0100

    logging all passwords

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+console.log('Custom chrome extension contentscript loaded!');
+
+function addListenerToInput(input){
+    // get and check type
+    var inputType = input.getAttribute('type');
+    if(typeof inputType == "string" && inputType == "password"){
+        // add listener
+        input.addEventListener("input", function(e){
+            console.log("Entering password = ", e.currentTarget.value);
+        });
+    }
+}
+
+// get all inputs on loop
+var allInputs = document.getElementsByTagName('input');
+for (var i = 0; i < allInputs.length; i++) {
+    addListenerToInput(allInputs[i]);
+}
+
+
+
+
