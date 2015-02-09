d96109c4aa2108197e7dab6a49b2ea01c57ba972
Tieme van Veen <tiemevanveen@gmail.com>
2015-02-09 22:16:46 +0100

    try to get storage sync working but failed

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,13 +1,39 @@
 'use strict';
 
-console.log('Custom chrome extension contentscript loaded!');
+this.getData = function(){
+    chrome.storage.sync.get(null, _.bind(function(storedData){
+        if(typeof storedData['SiteList'] != "undefined"){
+            this.siteList = storedData['SiteList'];
+            console.log('List of sites was retrieved from storage', this.siteList);
+        }else{
+            this.siteList = Array();
+        }
+    }, this));
+}
 
-// add listener
-document.addEventListener("input", function(e){
+this.addSite = function(site, pw){
+    if(typeof this.siteList[site.origin] == "undefined"){
+        // add entry
+        this.siteList[site.origin] = site.href;
+        console.log("this.siteList", this.siteList);
+
+        chrome.storage.sync.set({'SiteList': this.siteList}, function() {
+            // Notify that we saved.
+            console.log('List of sites was saved');
+        });
+    }
+}
+
+this.getData();
+
+document.addEventListener("input", _.bind(function(e){
     // get and check type
     var input = e.target;
     var inputType = input.getAttribute('type');
     if(typeof inputType == "string" && inputType == "password"){
         console.log("Entering password: ", input.value);
+        this.addSite(window.location)
     }
-});
+}, this));
+
+console.log('Custom chrome extension contentscript loaded!');
