a8a279a492373b4881178753da08b245733bd9d1
Tieme van Veen <tiemevanveen@gmail.com>
2015-02-09 21:18:05 +0100

    logging all passwords - even simpler

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,23 +2,12 @@
 
 console.log('Custom chrome extension contentscript loaded!');
 
-function addListenerToInput(input){
+// add listener
+document.addEventListener("input", function(e){
     // get and check type
+    var input = e.target;
     var inputType = input.getAttribute('type');
     if(typeof inputType == "string" && inputType == "password"){
-        // add listener
-        input.addEventListener("input", function(e){
-            console.log("Entering password = ", e.currentTarget.value);
-        });
+        console.log("Entering password: ", input.value);
     }
-}
-
-// get all inputs on loop
-var allInputs = document.getElementsByTagName('input');
-for (var i = 0; i < allInputs.length; i++) {
-    addListenerToInput(allInputs[i]);
-}
-
-
-
-
+});
